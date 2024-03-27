1ed4381e4318b3ccff03aae20d0e6dd4e97e35cd
khansk13 <137790753+khansk13@users.noreply.github.com>
2024-03-27 10:49:29 +0530

    commit

--- router/destinationRoutes.js
+++ router/destinationRoutes.js
@@ -4,8 +4,14 @@ const destinationController = require('../controller/destinationController');
 
 
 
-router.get('/get', destinationController.getDestinationsByAccountId);
-router.put('/update', destinationController.updateDestination);
-router.delete('/delete', destinationController.deleteDestination);
+router.get('/get', 
+        destinationController.getDestinationsByAccountId
+);
+router.put('/update',
+        destinationController.updateDestination
+);
+router.delete('/delete', 
+        destinationController.deleteDestination
+);
 
 module.exports = router;
